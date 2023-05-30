10afa4dff8076f04c5a6055acfae69afe24e399e
BanditCook <tanguy.lalexandre@gmail.com>
2023-05-30 22:47:59 +0100

    Add a start of logic for the attributes of the task

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,82 @@
+function createForm() {
+  // Create a form to fill. First thing to do is Enter the name of the task.
+  let formWrapper = document.querySelector("#taskManager");
+  let form = document.createElement("form");
+  form.classList.add("taskForm");
+  formWrapper.appendChild(form);
+
+  let taskName = document.createElement("input");
+  taskName.classList.add("taskName");
+  taskName.setAttribute("type", "text");
+  taskName.setAttribute("placeholder", "Enter your Task");
+  form.appendChild(taskName);
+  // Create a button to add a date.
+  let dateButton = document.createElement("button");
+  dateButton.classList.add("dateButton");
+  dateButton.textContent = "Add Date";
+  form.appendChild(dateButton);
+  // If the button is clicked, create a date input and a button to cancel the
+  dateButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    let clicked = false;
+    if (!clicked) {
+      let dateInput = document.createElement("input");
+      dateInput.classList.add("dateInput");
+      dateInput.setAttribute("type", "date");
+      form.appendChild(dateInput);
+      clicked = true;
+    } else {
+      return;
+    }
+
+    let removeDateInput = document.createElement("button");
+    removeDateInput.classList.add("removeDateInput");
+    removeDateInput.textContent = "-";
+    form.appendChild(removeDateInput);
+
+    removeDateInput.addEventListener("click", function (event) {
+      event.preventDefault();
+      let dateInput = document.querySelector(".dateInput");
+      dateInput.remove();
+      removeDateInput.remove();
+      let dateButton = document.createElement("button");
+      dateButton.classList.add(".dateButton");
+      dateButton.textContent = "Add Date";
+      form.appendChild(dateButton);
+    });
+  });
+
+  let addTaskButton = document.createElement("button");
+  addTaskButton.classList.add("addTaskButton");
+  addTaskButton.textContent = "+";
+  form.appendChild(addTaskButton);
+
+  addTaskButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    addTaskToList(task);
+  });
+
+  let removeTaskButton = document.createElement("button");
+  removeTaskButton.classList.add("removeTaskButton");
+  removeTaskButton.textContent = "-";
+  form.appendChild(removeTaskButton);
+
+  removeTaskButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    destroyForm();
+
+    console.log("je fonctionne");
+  });
+  function destroyForm() {
+    let taskForm = document.querySelector(".taskForm");
+    taskForm.remove();
+  }
+}
+
+newTask.addEventListener("click", function () {
+  createForm();
+});
+
 document
   .getElementById("taskForm")
   .addEventListener("submit", function (event) {
