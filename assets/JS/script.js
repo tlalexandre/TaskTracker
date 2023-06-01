86033a029655976a2d431c89b9cb3a2a59b37fc7
BanditCook <tanguy.lalexandre@gmail.com>
2023-06-01 10:55:48 +0100

    Add the logic of single task creation, add date to the task, and category system

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,11 +1,17 @@
 function createForm() {
   // Create a form to fill.
-  let formWrapper = document.querySelector("#taskManager");
   let form = document.createElement("form");
   form.classList.add("taskForm");
   form.classList.add("boxShadows");
   form.classList.add("gradientConvexe");
-  formWrapper.appendChild(form);
+  noCategory.appendChild(form);
+
+  let nameWrapper = document.createElement("div");
+  nameWrapper.classList.add("nameWrapper");
+  nameWrapper.classList.add("boxShadows");
+  nameWrapper.classList.add("gradientConvexe");
+  nameWrapper.textContent = "Name";
+  form.appendChild(nameWrapper);
 
   // Create the input for the name of the task.
   let taskName = document.createElement("input");
@@ -14,34 +20,29 @@ function createForm() {
   taskName.classList.add("gradientConcave");
   taskName.setAttribute("type", "text");
   taskName.setAttribute("placeholder", "Enter your Task");
-  form.appendChild(taskName);
+  nameWrapper.appendChild(taskName);
 
+  let dateWrapper = document.createElement("div");
+  dateWrapper.classList.add("dateWrapper");
+  dateWrapper.classList.add("boxShadows");
+  dateWrapper.classList.add("gradientConvexe");
+  dateWrapper.textContent = "Date";
+  form.appendChild(dateWrapper);
   // Create a button to add a date.
-  let dateButton = document.createElement("button");
-  dateButton.classList.add("dateButton");
-  dateButton.classList.add("boxShadows");
-  dateButton.classList.add("gradientConcave");
-  dateButton.textContent = "Add Date";
-  form.appendChild(dateButton);
+  // let dateButton = document.createElement("button");
+  // dateButton.classList.add("dateButton");
+  // dateButton.classList.add("boxShadows");
+  // dateButton.classList.add("gradientConcave");
+  // dateButton.textContent = "Add Date";
+  // dateWrapper.appendChild(dateButton);
 
-  let clicked = false;
-  // If the button is clicked, create a date input.
-  dateButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    if (!clicked) {
-      let dateInput = document.createElement("input");
-      dateInput.type = "date";
-      dateInput.classList.add("dateInput");
-      form.appendChild(dateInput);
-      let labelDate = document.createElement("label");
-      labelDate.setAttribute("for", dateInput);
-      form.appendChild(labelDate);
-      clicked = true;
-      dateButton.remove();
-    } else {
-      return;
-    }
-  });
+  let dateInput = document.createElement("input");
+  dateInput.type = "date";
+  dateInput.value = Date();
+  dateInput.classList.add("dateInput");
+  dateInput.classList.add("boxShadows");
+  dateInput.classList.add("gradientConvexe");
+  dateWrapper.appendChild(dateInput);
 
   let addTaskButton = document.createElement("button");
   addTaskButton.classList.add("addTaskButton");
@@ -49,14 +50,28 @@ function createForm() {
   addTaskButton.classList.add("gradientConcave");
   addTaskButton.textContent = "Add Task";
   form.appendChild(addTaskButton);
+
   addTaskButton.addEventListener("click", function (event) {
     event.preventDefault();
     let name = taskName.value;
     let nameContainer = document.createElement("h2");
+    let date = dateInput.value;
+
+    let dateContainer = document.createElement("p");
     nameContainer.classList.add("nameContainer");
     nameContainer.textContent = name;
+    dateContainer.classList.add("dateContainer");
+    if (date === "") {
+      date = "Undefined Date";
+    }
+    dateContainer.textContent = date;
+
+    nameWrapper.appendChild(nameContainer);
+    dateWrapper.appendChild(dateContainer);
     taskName.remove();
-    form.appendChild(nameContainer);
+    dateInput.remove();
+    addTaskButton.remove();
+    open = false;
   });
   let removeTaskButton = document.createElement("button");
   removeTaskButton.classList.add("removeTaskButton");
@@ -68,50 +83,53 @@ function createForm() {
   removeTaskButton.addEventListener("click", function (event) {
     event.preventDefault();
     destroyForm();
-
-    console.log("je fonctionne");
   });
   function destroyForm() {
     let taskForm = document.querySelector(".taskForm");
+    open = false;
     taskForm.remove();
   }
 }
 
+let open = false;
 newTask.addEventListener("click", function () {
-  createForm();
+  if (open == false) {
+    createForm();
+    open = true;
+  } else {
+    alert("Please complete the actual field before opening a new one.");
+  }
 });
 
-document
-  .getElementById("taskForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent form submission and page reload
+function createCategory() {
+  let addCategoryButton = document.createElement("input");
+  addCategoryButton.classList.add("addCategory");
+  addCategoryButton.classList.add("boxShadows");
+  addCategoryButton.classList.add("gradientConcave");
+  addCategoryButton.setAttribute("type", "text");
+  addCategoryButton.setAttribute("placeholder", "Give a name to your category");
+  categories.appendChild(addCategoryButton);
 
-    // Get task details from form inputs
-    var taskName = document.getElementById("taskName").value;
-    var dueDate = document.getElementById("timeDue").value;
-    var description = document.getElementById("taskDescription").value;
-
-    // Create new task object
-    var task = {
-      name: taskName,
-      date: dueDate,
-      description: description,
-      status: "pending",
-    };
-    function addTaskToList(task) {
-      let taskList = document.getElementById("taskList");
-      taskList.innerHTML = `<div class='task'>
-            <h1>${task.name}</h1>  
-            <p>${task.description}</p>
-            <span>${task.date}</span>
-      </div>`;
+  addCategoryButton.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      let titleCategory = addCategoryButton.value;
+      let category = document.createElement("ul");
+      category.textContent = titleCategory;
+      category.classList.add(`${titleCategory}`);
+      category.classList.add("boxShadows");
+      category.classList.add("gradientConcave");
+      categories.appendChild(category);
+      addCategoryButton.remove();
+      open = false;
     }
-
-    // Add the new task to the task list
-    addTaskToList(task);
-
-    // Clear form inputs
-    document.getElementById("taskName").value = "";
-    document.getElementById("timeDue").value = "";
-    document.getElementById("taskDescription").value = "";
   });
+}
+
+newCategory.addEventListener("click", function () {
+  if (open == false) {
+    createCategory();
+    open = true;
+  } else {
+    alert("Please complete the actual field before opening a new one.");
+  }
+});
