2f4435e04a0a6b09e10d65b3c73c5ecd052050b0
BanditCook <tanguy.lalexandre@gmail.com>
2023-06-06 13:08:02 +0100

    Add the new logic to the form, add tasks

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,202 +1,97 @@
-function createForm() {
-  // Create a form to fill.
-  let form = document.createElement("form");
-  let taskCreation = document.querySelector(".taskCreation");
-
-  let selectedCategory = null;
-
-  form.classList.add("taskForm");
-  form.classList.add("boxShadows");
-  form.classList.add("gradientConvexe");
-  taskCreation.appendChild(form);
-
-  let nameWrapper = document.createElement("div");
-  nameWrapper.classList.add("nameWrapper");
-  form.appendChild(nameWrapper);
-
-  let nameText = document.createElement("h2");
-  nameText.classList.add("nameText");
-  nameText.textContent = "Name";
-  nameWrapper.appendChild(nameText);
-
-  // Create the input for the name of the task.
-  let taskName = document.createElement("input");
-  taskName.classList.add("taskName");
-  taskName.classList.add("boxShadows");
-  taskName.classList.add("gradientConcave");
-  taskName.setAttribute("type", "text");
-  taskName.setAttribute("placeholder", "Enter your Task");
-  nameWrapper.appendChild(taskName);
-
-  let dateWrapper = document.createElement("div");
-  dateWrapper.classList.add("dateWrapper");
-  form.appendChild(dateWrapper);
-
-  let dateText = document.createElement("h2");
-  dateText.classList.add("dateText");
-  dateText.textContent = "Date";
-  dateWrapper.appendChild(dateText);
-
-  let dateInput = document.createElement("input");
-  dateInput.type = "date";
-  dateInput.value = Date();
-  dateInput.classList.add("dateInput");
-  dateInput.classList.add("boxShadows");
-  dateInput.classList.add("gradientConvexe");
-  dateWrapper.appendChild(dateInput);
-
-  let categoriesPick = document.createElement("div");
-  categoriesPick.classList.add("categoriesPick");
-  form.appendChild(categoriesPick);
-
-  let categoriesPickText = document.createElement("h2");
-  categoriesPickText.classList.add("categoriesPickText");
-  categoriesPickText.textContent = "Categories";
-  categoriesPick.appendChild(categoriesPickText);
-
-  let categoriesPickUl = document.createElement("ul");
-  categoriesPickUl.classList.add("categoriesPickUl");
-
-  categoriesPick.appendChild(categoriesPickUl);
-
-  let categories = retrieveCategories();
-
-  categories.forEach(function (category) {
-    let categoryLi = document.createElement("li");
-    let categoryButton = document.createElement("button");
-    categoryButton.classList.add("categoryButton");
-    categoryButton.classList.add("boxShadows");
-
-    categoryLi.appendChild(categoryButton);
-    categoryButton.textContent = category;
-    categoriesPickUl.appendChild(categoryLi);
-
-    categoryButton.addEventListener("click", function (e) {
-      e.preventDefault();
-      selectedCategory = category;
-    });
-  });
-  let buttonContainer = document.createElement("div");
-  buttonContainer.classList.add("buttonContainer");
-  form.appendChild(buttonContainer);
-
-  let addTaskButton = document.createElement("button");
-  addTaskButton.classList.add("addTaskButton");
-  addTaskButton.classList.add("boxShadows");
-  addTaskButton.classList.add("gradientConcave");
-  addTaskButton.textContent = "Add Task";
-  buttonContainer.appendChild(addTaskButton);
-
-  addTaskButton.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    if (selectedCategory) {
-      let name = taskName.value;
-      let date = dateInput.value;
-
-      let task = document.createElement("li");
-      task.classList.add("boxShadows");
-      let taskNameElement = document.createElement("h2");
-      let taskDetails = document.createElement("div");
-      let taskDate = document.createElement("p");
-
-      taskNameElement.textContent = name;
-      taskDate.textContent = "Due for: " + (date || "Undefined Date");
-
-      task.appendChild(taskNameElement);
-      taskDetails.appendChild(taskDate);
-      task.appendChild(taskDetails);
-
-      let categoryElement = document.querySelector(`.` + `${selectedCategory}`);
-      console.log(categoryElement);
-      categoryElement.querySelector("ul").appendChild(task);
-
-      taskName.remove();
-      dateInput.remove();
-      addTaskButton.remove();
-      selectedCategory = null;
-      open = false;
-    } else {
-      alert("Please select a category for the task.");
-    }
-  });
-  let removeTaskButton = document.createElement("button");
-  removeTaskButton.classList.add("removeTaskButton");
-  removeTaskButton.classList.add("boxShadows");
-  removeTaskButton.classList.add("gradientConcave");
-  removeTaskButton.textContent = "Remove";
-  buttonContainer.appendChild(removeTaskButton);
-
-  removeTaskButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    destroyForm();
-  });
-  function destroyForm() {
-    let taskForm = document.querySelector(".taskForm");
-    open = false;
-    taskForm.remove();
-  }
-}
-
-let open = false;
-newTask.addEventListener("click", function () {
-  if (open == false) {
-    createForm();
-    open = true;
-  } else {
-    alert("Please complete the actual field before opening a new one.");
-  }
-});
+let addCategory = document.querySelector(".addCategory");
 let categoriesWrapper = document.querySelector(".categoriesWrapper");
 
 function createCategory() {
-  let addCategoryButton = document.createElement("input");
-  addCategoryButton.classList.add("addCategory");
-  addCategoryButton.classList.add("boxShadows");
-  addCategoryButton.classList.add("gradientConcave");
-  addCategoryButton.setAttribute("type", "text");
-  addCategoryButton.setAttribute("placeholder", "Give a name to your category");
-  categoriesWrapper.appendChild(addCategoryButton);
+  let newCategory = document.createElement("div");
+  newCategory.classList.add("newCategory");
+  categoriesWrapper.appendChild(newCategory);
 
-  addCategoryButton.addEventListener("keypress", function (e) {
-    if (e.key === "Enter") {
-      let titleCategory = addCategoryButton.value;
-      let category = document.createElement("div");
-      let categoryTitle = document.createElement("h2");
-      let categoryUl = document.createElement("ul");
+  let categoryName = document.createElement("input");
+  categoryName.classList.add("categoryNameInput");
+  categoryName.setAttribute("type", "text");
+  categoryName.setAttribute("placeholder", `"Category Name"`);
+  newCategory.appendChild(categoryName);
 
-      category.classList.add(`${titleCategory}`);
-      category.classList.add(`${titleCategory}`);
-      category.classList.add("boxShadows");
-      category.classList.add("gradientConcave");
+  let addNewTask = document.createElement("button");
+  addNewTask.classList.add("addNewTask");
+  addNewTask.textContent = "New Task";
+  newCategory.appendChild(addNewTask);
 
-      category.appendChild(categoryTitle);
-      category.appendChild(categoryUl);
-
-      categoryTitle.textContent = `${titleCategory}`;
-      categoriesWrapper.appendChild(category);
-      addCategoryButton.remove();
-      open = false;
+  categoryName.addEventListener("keyup", (event) => {
+    if (event.key == "Enter") {
+      setCategoryName(categoryName, newCategory);
     }
+  });
+  categoriesWrapper.insertBefore(newCategory, categoriesWrapper.firstChild);
+
+  addNewTask.addEventListener("click", () => {
+    createTask(newCategory, addNewTask);
   });
 }
 
-newCategory.addEventListener("click", function () {
-  if (open == false) {
-    createCategory();
-    open = true;
-  } else {
-    alert("Please complete the actual field before opening a new one.");
-  }
+addCategory.addEventListener("click", () => {
+  createCategory();
 });
 
-function retrieveCategories() {
-  let categories = [];
-  let categoriesElements = document.querySelectorAll(".categoriesWrapper>div");
+function setCategoryName(categoryNameInput, newCategory) {
+  let categoryNameValue = categoryNameInput.value;
+  let categoryNameHeading = document.createElement("h2");
+  categoryNameHeading.classList.add("categoryName");
+  categoryNameHeading.textContent = categoryNameValue;
 
-  categoriesElements.forEach(function (categoriesElement) {
-    let categoryTitle = categoriesElement.querySelector("h2").textContent;
-    categories.push(categoryTitle);
+  newCategory.insertBefore(categoryNameHeading, newCategory.firstChild);
+  categoryNameInput.remove();
+}
+
+function createTask(category, addNewTask) {
+  let newTask = document.createElement("div");
+  newTask.classList.add("newTask");
+
+  let taskDetails = document.createElement("div");
+  taskDetails.classList.add("taskDetails");
+  newTask.appendChild(taskDetails);
+
+  let taskName = document.createElement("input");
+  taskName.classList.add("taskNameInput");
+  taskName.setAttribute("type", "text");
+  taskName.setAttribute("placeholder", `"Task Name"`);
+  newTask.appendChild(taskName);
+
+  let taskDate = document.createElement("input");
+  taskDate.classList.add("taskDateInput");
+  taskDate.setAttribute("type", "date");
+  newTask.appendChild(taskDate);
+
+  taskName.addEventListener("keyup", (event) => {
+    if (event.key == "Enter") {
+      addTaskNameToTask(taskName.value, taskDetails);
+      taskName.remove();
+      taskDate.focus();
+    }
   });
-  return categories;
+
+  taskDate.addEventListener("keyup", (event) => {
+    if (event.key == "Enter") {
+      addTaskDateToTask(taskDate.value, taskDetails);
+      taskDate.remove();
+    }
+  });
+
+  category.insertBefore(newTask, addNewTask);
+}
+
+function addTaskNameToTask(taskName, taskDetails) {
+  let task = document.createElement("h3");
+  task.classList.add("taskName");
+  task.textContent = taskName;
+
+  taskDetails.insertBefore(task, taskDetails.firstChild);
+}
+
+function addTaskDateToTask(taskDate, taskDetails) {
+  let date = document.createElement("p");
+  date.classList.add("taskDate");
+  date.textContent = taskDate;
+
+  taskDetails.appendChild(date);
 }
